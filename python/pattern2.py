53130e27c8505f70fb1d7b551b208b088f1ffa61
Dhruvi Gajjar <84338392+dhruvii16@users.noreply.github.com>
2024-02-01 18:46:28 +0530

    Add files via upload

---
+++ python/pattern2.py
@@ -0,0 +1,58 @@
+rows = 4
+
+for i in range(4,0,-1):
+	for j in range(0,i):
+		print('*',end='')
+	print('')
+
+
+rows = 3
+
+for i in range(3,0,-1):
+	for j in range(0,i):
+		print(i,end='')
+	print('')
+
+
+rows = 5
+for i in range(1,6):
+
+    for j in range(5 - i):
+        print(" ", end="")
+
+    for k in range(1,i + 1):
+        print('* ' , end="")
+    print('')
+
+
+rows = 5
+for i in range(1,6):
+
+    for j in range(5- i):
+        print(" ", end="")
+
+    for k in range(1,i + 1):
+        print('*' , end="")
+    print('')
+
+
+rows = 5
+for i in range(1,6):
+
+    for j in range(5 - i):
+        print(" ", end="")
+
+    for k in range(1,i + 1):
+        print(k, end="")
+    print('')
+
+
+for i in range(1,6):
+    for j in range(5 - i):
+        print(" ", end="")
+    for k in range(1, i + 1):
+        print(k, end=" ")
+   
+    print('')
+
+
